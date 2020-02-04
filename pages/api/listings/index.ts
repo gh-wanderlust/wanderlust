b61607a553bea61cee802387ab06cb36c6fb0fa2
Mika Busante <mika@mikabusante.com>
2020-02-04 16:50:31 -0500

    bug: fix deploy bug -- saving before installing possible package fix

---
+++ pages/api/listings/index.ts
@@ -0,0 +1,14 @@
+const { Listing } = require("../../../server/db/models");
+const { NextApiRequest, NextApiResponse } = require("next");
+
+export default async (req: any, res: any) => {
+  if (req.method === "GET") {
+    const listings = await Listing.findAll();
+    res.json(listings);
+  }
+
+  if (req.method === "POST") {
+    const listing = await Listing.create(req.body);
+    res.status(201).json(listing);
+  }
+};

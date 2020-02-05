2dabc9c0ecd71936b565fe980b958a6cfdc288e6
Rowena Chen <rwnchen@gmail.com>
2020-02-05 15:18:09 -0500

    Clean: Cleared up some comments from db file

--- server/db/database.js
+++ server/db/database.js
@@ -1,12 +1,4 @@
 const Sequelize = require('sequelize');
-// const pkg = require('../../package.json');
-// const databaseName =
-//   pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
-
-// // if deploying via heroku, add "process.env.DATABASE_URL" to the Sequelize instance
-// const db = new Sequelize(`postgres://localhost:5432/${databaseName}`, {
-//   logging: false,
-// });
 
 const db = new Sequelize(
   'postgres',
@@ -19,23 +11,5 @@ const db = new Sequelize(
     logging: false,
   }
 );
-
-// const Sequelize = require('sequelize')
-// require('dotenv').config()
-
-
-
-// const db = new Sequelize(
-//         'postgres',
-//         process.env.SQL_GHP_WANDERLUST_USERNAME,
-//         process.env.SQL_GHP_WANDERLUST_PASSWORD,
-//         {
-
-//             // this is the IP address
-//           host: process.env.SQL_GHP_WANDERLUST_IP,
-//           dialect: 'postgres',
-
-//         },
-//       );
 
 module.exports = db;

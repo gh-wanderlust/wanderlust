c7baf99aa557d62a383ee4b52c77277663195976
Rowena Chen <rwnchen@gmail.com>
2020-02-06 17:17:30 -0500

    [Feature] Added html skeleton with dummy data in place

--- pages/listings/[id].tsx
+++ pages/listings/[id].tsx
@@ -1,69 +1,134 @@
-import React from 'react'
-import axios from 'axios'
-import { useRouter } from 'next/router'
+import React from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 interface ListingData {
-    name: string,
-    description: string,
-    address: string,
-    city: string,
-    country: string
+  name: string;
+  description: string;
+  address: string;
+  city: string;
+  country: string;
 }
 
 interface ListingProps {
-    listing: object
+  listing: object;
 }
 
-const dummyListing = {
-    name: 'Peaceful Log Cabin in the Woods',
-    description:
-      'This log cabin is set in the woods in a rural part of northeastern Vermont. Escape the hustle and bustle, clear your mind, and enjoy nature. A great place to get some fresh air or to stay in and take a nap. Beautiful summers for easy hikes and refreshing swims in the lakes of our local Groton State Forest, unbelievable foliage to view from small dirt roads, and tons of outdoor winter activities. Great for a couples getaway, friends weekend, or some quality time with the kids. Pets welcome, too!',
-    address: '888 Loch Ness Way',
-    city: 'Inverness',
-    country: 'Scotland',
-    minOccupants: 4,
-    maxOccupants: 8,
-    ownerPhotos: [
-      'https://roadesque.com/assets/a-peaceful-cabin-in-the-canadian-woods-at-logden-lodge/logdenLodge-2.jpg',
-      'https://i.pinimg.com/originals/51/5e/8e/515e8e2f8de4ce6b42f6aaebbf923543.jpg',
-      'https://www.impressiveinteriordesign.com/wp-content/uploads/2012/10/Cabin-Design-Ideas-For-Inspiration-7.jpg',
-    ]
-  }
+const dummyUser = {
+  id: 1,
+  firstName: 'William',
+  lastName: 'Wallace',
+  email: 'toFreedom@scotland.com',
+  imageUrl:
+    'https://www.britain-magazine.com/wp-content/uploads/William-Wallace.jpg',
+  createdAt: '2020-02-06T14:39:56.062Z',
+  updatedAt: '2020-02-06T14:39:56.062Z',
+};
+
+const dummyDate = {
+  dateFrom: new Date(Date.now()),
+  dateTo: new Date(Date.now()),
+};
+
+const dummyData = {
+  id: 2,
+  name: "Howl's Moving Castle",
+  description: 'Charming castle on the move with charming prince!',
+  address: '1986 Wynne Jones Road',
+  city: 'Osaka',
+  country: 'Japan',
+  minOccupants: 3,
+  maxOccupants: 6,
+  ownerPhotos: [
+    'https://i.imgur.com/bMpCKQ3.jpeg',
+    'https://i.imgur.com/d8zr5a9.jpeg',
+    'https://img.buzzfeed.com/buzzfeed-static/static/2015-07/15/17/enhanced/webdr05/original-9377-1436995231-3.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto',
+  ],
+  createdAt: '2020-02-06T14:39:55.890Z',
+  updatedAt: '2020-02-06T14:39:55.890Z',
+  trips: [
+    {
+      id: 1,
+      dateFrom: '2021-04-25',
+      dateTo: '2021-05-05',
+      status: 'pending',
+      createdAt: '2020-02-06T14:39:56.159Z',
+      updatedAt: '2020-02-06T14:39:56.224Z',
+      listingId: 2,
+      users: [
+        {
+          id: 3,
+          firstName: 'Sophie',
+          lastName: 'Hatter',
+          email: 'sophie@hatter.com',
+          imageUrl:
+            'https://i.pinimg.com/originals/a8/10/7a/a8107a0f1afa49f51f11566843830a55.jpg',
+          createdAt: '2020-02-06T14:39:56.061Z',
+          updatedAt: '2020-02-06T14:39:56.061Z',
+          UserTrip: {
+            createdAt: '2020-02-06T14:39:56.294Z',
+            updatedAt: '2020-02-06T14:39:56.294Z',
+            userId: 3,
+            tripId: 1,
+          },
+        },
+        {
+          id: 2,
+          firstName: 'Dwayne The Rock',
+          lastName: 'Johnson',
+          email: 'yourWelcome@moana.com',
+          imageUrl:
+            'https://upload.wikimedia.org/wikipedia/commons/f/f1/Dwayne_Johnson_2%2C_2013.jpg',
+          createdAt: '2020-02-06T14:39:56.062Z',
+          updatedAt: '2020-02-06T14:39:56.062Z',
+          UserTrip: {
+            createdAt: '2020-02-06T14:39:56.294Z',
+            updatedAt: '2020-02-06T14:39:56.294Z',
+            userId: 2,
+            tripId: 1,
+          },
+        },
+      ],
+    },
+  ],
+};
 
 const SingleListing = () => {
-
-    return (
-        <div>
-            <div>
-                {dummyListing.ownerPhotos.map(imgUrl => {
-                    return (
-                        <img src={imgUrl}/>
-                    )
-                })}
-            </div>
-            <div>
-                <h2>{dummyListing.name}</h2>
-                <p>{dummyListing.description}</p>
-            </div>
-            <div>
-                
-            </div>
-            <button>I'm interested!</button>
-        </div>
-
-    )
-
-}
+  return (
+    <div>
+      <div>
+        {dummyData.ownerPhotos.map((imgUrl) => {
+          return <img src={imgUrl} />;
+        })}
+      </div>
+      <div>
+        <h2>{dummyData.name}</h2>
+        <p>{dummyData.description}</p>
+      </div>
+      <div>
+        <ul>
+          {dummyData.trips.map((trip) => {
+            if (trip.status === 'pending') {
+              return trip.users.map((user) => {
+                return <li>{`${user.firstName} ${user.lastName}`}</li>;
+              });
+            }
+          })}
+        </ul>
+        <button>I'm interested!</button>
+      </div>
+    </div>
+  );
+};
 
 SingleListing.getInitialProps = async () => {
-    const router = useRouter()
-    const { id } = router.query
-    const res = await axios.get(`https://localhost:3000/api/listings/${id}`)
-    console.log("RES: ", res)
-    const listing = res.data
+  const router = useRouter();
+  const { id } = router.query;
+  const res = await axios.get(`https://localhost:3000/api/listings/${id}`);
+  console.log('RES: ', res);
+  const listing = res.data;
 
-    return { listing }
-}
+  return { listing };
+};
 
-
-export default SingleListing
+export default SingleListing;

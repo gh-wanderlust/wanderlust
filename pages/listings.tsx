e4b29bfb0b2a446a427b685e8f5262b831d45f12
Mika Busante <mika@mikabusante.com>
2020-02-06 17:03:32 -0500

    bug: fix getInitialProps in page components -- add missing code block to _app

--- pages/listings.tsx
+++ pages/listings.tsx
@@ -1,30 +1,29 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import axios from 'axios';
-import { getListings } from '../store/store'
+import React from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import { getListings } from "../store/store";
 
 const Listings = (props: any) => {
-    console.log("PROPS:", props)
-        return (
-            <div>
-            <p>Listings</p>
-            </div>
-        )
-}
+  console.log("PROPS:", props);
+  return (
+    <div>
+      <p>Listings</p>
+    </div>
+  );
+};
 
-const mapStateToProps = (state: any) => {
-    return {
-        data: state
-    }
-}
+// const mapStateToProps = (state: any) => {
+//   return {
+//     data: state
+//   };
+// };
 
-Listings.getInitialProps = async function({ store }: any){
-    // const res = await axios.get('https://api.tvmaze.com/search/shows?q=batman');
-    // console.log(res)
+Listings.getInitialProps = async function() {
+  const res = await axios.get("https://api.tvmaze.com/search/shows?q=batman");
 
-    // return res
-    await store.dispatch(getListings())
-    console.log("HELLO")
-}
+  return { res: res.data };
+  //   await store.dispatch(getListings());
+  //   console.log("HELLO");
+};
 
-export default connect(mapStateToProps, null)(Listings)
+export default connect()(Listings);

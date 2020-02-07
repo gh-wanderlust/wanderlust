5091b7fbe22e12709a32cabe2611b6362cda0e9b
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-02-07 12:47:57 -0500

    refactor: listing data moved to its own file

---
+++ listingSeedData.js
@@ -0,0 +1,54 @@
+const listingSeed = [
+	{
+		name: 'Peaceful Log Cabin in the Woods',
+		description:
+			'This log cabin is set in the woods in a rural part of Montpelier. Escape the hustle and bustle, clear your mind, and enjoy nature. A great place to get some fresh air or to stay in and take a nap. Beautiful summers for easy hikes and refreshing swims in the lakes of our local Groton State Forest, unbelievable foliage to view from small dirt roads, and tons of outdoor winter activities. Great for a couples getaway, friends weekend, or some quality time with the kids. Pets welcome, too!',
+		address: '67 Bliss Rd',
+		city: 'Montpelier',
+		country: 'United States of America',
+		zipCode: 05602,
+		minOccupants: 4,
+		maxOccupants: 8,
+		ownerPhotos: [
+			'https://roadesque.com/assets/a-peaceful-cabin-in-the-canadian-woods-at-logden-lodge/logdenLodge-2.jpg',
+			'https://i.pinimg.com/originals/51/5e/8e/515e8e2f8de4ce6b42f6aaebbf923543.jpg',
+			'https://www.impressiveinteriordesign.com/wp-content/uploads/2012/10/Cabin-Design-Ideas-For-Inspiration-7.jpg'
+		],
+		price: 20000
+	},
+	{
+		name: "Howl's Moving Castle",
+		description: 'Charming castle on the move with charming prince!',
+		address: '1986 Wynne Jones Road',
+		city: 'Osaka',
+		country: 'Japan',
+		zipCode: 00000,
+		minOccupants: 3,
+		maxOccupants: 6,
+		ownerPhotos: [
+			'https://i.imgur.com/bMpCKQ3.jpeg',
+			'https://i.imgur.com/d8zr5a9.jpeg',
+			'https://img.buzzfeed.com/buzzfeed-static/static/2015-07/15/17/enhanced/webdr05/original-9377-1436995231-3.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto'
+		],
+		price: 2000
+	},
+	{
+		name: 'Bungalow over water',
+		description: 'Beautiful bungalow for your next getaway!',
+		address: '123 Hakuna Matata Road',
+		city: 'Bora Bora',
+		country: 'French Polynesia',
+		zipCode: 00000,
+		minOccupants: 3,
+		maxOccupants: 6,
+		ownerPhotos: [
+			'https://www.jetsetter.com/uploads/sites/7/2018/09/tgBIPKMb-1380x690.jpeg',
+			'https://i.pinimg.com/originals/eb/db/dd/ebdbdd76d7ed4b55cd4dae8702b8bded.jpg',
+			'https://www.jetsetter.com/uploads/sites/7/2018/04/bath-hotels-island-luxury-960x960.jpeg',
+			'https://www.fourseasons.com/alt/img-opt/~70.1530/publish/content/dam/fourseasons/images/web/BOR/BOR_130_aspect16x9.jpg'
+		],
+		price: 20000
+	}
+];
+
+  module.exports = listingSeed

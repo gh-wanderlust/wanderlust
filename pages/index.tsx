a8a6b4ac2938f6b15164b29152f018c349b3012b
Joanna Huang <joannathhuang@gmail.com>
2020-02-09 17:18:22 -0500

    [Feat]: basic structure for landing page with input fields and image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getTest } from "../store/store";
+import styled from 'styled-components'
 
 interface LinkStateProps {
   state: Array<string>;
@@ -15,10 +16,24 @@ type Props = LinkStateProps & LinkDispatchProps;
 class Index extends React.Component<Props> {
   render() {
     return (
-      <div>
-        <p>Hello!</p>
-        <button onClick={() => this.props.testDispatch()}>TESTSCRIPT</button>
-      </div>
+      <Wrapper>
+
+        <p>Logo</p>
+        <h1>Find your next adventure.</h1>
+        <form>
+          Where
+          <input type="text" name="where"></input>
+          From
+          <input type="date" name="fromDate"></input>
+          To
+          <input type="date" name="toDate"></input>
+
+        </form>
+        <Button onClick={() => this.props.testDispatch()}>Search</Button>
+        <div>
+          <HeroImg alt="heroImg" src="https://c0.wallpaperflare.com/preview/732/704/957/mountain-snow-house-hillside.jpg"/>
+        </div>
+      </Wrapper>
     );
   }
 }
@@ -31,3 +46,39 @@ const mapDispatchToProps = (dispatch: any) => ({
   testDispatch: () => dispatch(getTest())
 });
 export default connect(mapStateToProps, mapDispatchToProps)(Index);
+
+
+const Wrapper = styled.div`
+  padding: 1em;
+  border: black;
+  background: #purple;
+`;
+
+const HeroImg = styled.img`
+  height: 50%;
+  width: 50%;
+`;
+
+const GuestPhotos = styled.div`
+  display: flex;
+  width: 97vw;
+`;
+const ProfilePic = styled.img`
+  width: 55px;
+  height: 55px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const Button = styled.button`
+  background: green;
+  color: white;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid darkgreen;
+  border-radius: 3px;
+`;
+
+
+

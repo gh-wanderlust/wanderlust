065d72d18aa2055d9b6fdb8334c55d4c08266d78
Joanna Huang <joannathhuang@gmail.com>
2020-02-10 12:44:22 -0500

    Feat: got homepage in relative position, working on finetuning spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,130 @@
-import React from "react";
-import { connect } from "react-redux";
-// import { getTest } from "../store/store";
+import React from 'react'
+import {connect} from 'react-redux'
 import styled from 'styled-components'
+import Link from 'next/link'
+// import { getTest } from "../store/store";
 
 interface LinkStateProps {
-  state: Array<string>;
+  state: Array<string>
 }
 
 interface LinkDispatchProps {
-  testDispatch: () => void;
+  submitSearch: () => void
 }
 
-type Props = LinkStateProps & LinkDispatchProps;
+type Props = LinkStateProps & LinkDispatchProps
 
 class Index extends React.Component<Props> {
+  // constructor(props: Props) {
+  //   super(props)
+  //   this.handleChange = this.handleChange.bind(this)
+  // }
+
+  // handleChange = (e: any) => {
+  //   console.log('EVENT TARGET: ', e.target.value)
+  // }
+
   render() {
     return (
-      <Wrapper>
-
-        <p>Logo</p>
-        <h1>Find your next adventure.</h1>
-        <form>
-          Where
-          <input type="text" name="where"></input>
-          From
-          <input type="date" name="fromDate"></input>
-          To
-          <input type="date" name="toDate"></input>
-
-        </form>
-        <Button onClick={() => this.props.testDispatch()}>Search</Button>
+      <div>
+        <SearchWrapper>
+          <p>Logo</p>
+          <h1>Find your next adventure.</h1>
+          <SearchForm>
+            Select your destination
+            <select name="cities" id="cities">
+              <option value="anywhere">Anywhere</option>
+              <option value="osaka">Osaka</option>
+              <option value="bora bora">Bora Bora</option>
+              <option value="inverness">Inverness</option>
+            </select>
+            From
+            <input type="date" name="fromDate"></input>
+            To
+            <input type="date" name="toDate"></input>
+          </SearchForm>
+          <SearchButton onClick={() => this.props.submitSearch()}>
+            Search
+          </SearchButton>
+        </SearchWrapper>
         <div>
-          <HeroImg alt="heroImg" src="https://c0.wallpaperflare.com/preview/732/704/957/mountain-snow-house-hillside.jpg"/>
+          <HeroImg
+            alt="heroImg"
+            src="https://c0.wallpaperflare.com/preview/732/704/957/mountain-snow-house-hillside.jpg"
+          />
         </div>
-      </Wrapper>
-    );
+        <LoginButtonWrapper>
+          <LoginButton>Log In</LoginButton>
+          <LoginButton>Sign Up</LoginButton>
+        </LoginButtonWrapper>
+      </div>
+    )
   }
 }
 
 const mapStateToProps = (state: Array<string>) => ({
-  state: state,
-});
+  state: state
+})
 
 const mapDispatchToProps = (dispatch: any) => ({
-  testDispatch: () => {},
-});
-export default connect(mapStateToProps, mapDispatchToProps)(Index);
+  submitSearch: () => {}
+})
 
+export default connect(mapStateToProps, mapDispatchToProps)(Index)
 
-const Wrapper = styled.div`
-  padding: 1em;
-  border: black;
-  background: #purple;
-`;
+const SearchWrapper = styled.div`
+  font-family: 'Lucida Console', sans-serif;
+  padding: 50px;
+  background: #FFFFFF;
+  height: 100%;
+  width: 28%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`
 
-const HeroImg = styled.img`
-  height: 50%;
-  width: 50%;
-`;
+const SearchForm = styled.form`
+  display: grid;
+  position: absolute;
+  top: 200px;
+  left: 50px;
+`
 
-const GuestPhotos = styled.div`
-  display: flex;
-  width: 97vw;
-`;
-const ProfilePic = styled.img`
-  width: 55px;
-  height: 55px;
-  border-radius: 50%;
-  object-fit: cover;
-`;
-
-const Button = styled.button`
-  background: green;
+const SearchButton = styled.button`
+  background: #23565c;
   color: white;
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
   border: 2px solid darkgreen;
   border-radius: 3px;
-`;
+  position: absolute;
+  top: 350px;
+  left: 35px;
+`
 
+const LoginButtonWrapper = styled.button`
+  background: transparent;
+  border: none;
+  position: absolute;
+  top: 30px;
+  right: 50px;
+`
 
+const LoginButton = styled.button`
+  background: transparent;
+  color: white;
+  font-size: 15px;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: none;
+  border-radius: 3px;
+`
 
+const HeroImg = styled.img`
+  height: 100%;
+  width: 65%;
+  position: absolute;
+  left: 35%;
+  top: 0;
+`

087180fc731f9cd50c545e91b4222fcd639856ed
Joanna Huang <joannathhuang@gmail.com>
2020-02-10 17:13:01 -0500

    Feat: user profile --outward view

---
+++ pages/users/[id].tsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+const UserProfile = function(props: any) {
+    const user = props.user
+    console.log("OUR USER:  ", user)
+
+    return (
+        <div>
+          <h1>{`${user.firstName} ${user.lastName}`}</h1>
+          <img src={user.imageUrl}/>  
+          <p>{user.email}</p>
+          <h2>Interested Listings:</h2>
+          {user.trips.map((trip:any) => {
+              if (trip.status === "pending") {
+                  const interestedListing = user.listings.filter((listing:any) => listing.id === trip.listingId)
+                  return <div>{interestedListing[0].name}</div>
+              }
+          })}
+        </div>
+    )
+}
+
+UserProfile.getInitialProps = async(context:any) => {
+    const id = context.query.id
+    const res = await axios.get(`http://localhost:3000/api/users/${id}`)
+    return { user: res.data }
+}
+
+export default UserProfile;

a64eab6bfb472971a23660bff8d9e74fb9fc2bf3
Joanna Huang <joannathhuang@gmail.com>
2020-02-13 11:25:25 -0500

    [Fix]: updated util index file conditional to check for NODE_ENV instead

--- util/index.ts
+++ util/index.ts
@@ -1,8 +1,11 @@
 export const apiUrl = (slug: string) => {
   let base;
-  if (process.env.DEPLOY) {
-    base = 'https://wanderlust-git-deployment.gh-wanderlust.now.sh/';
-  } else base = 'http://localhost:3000';
+  if (process.env.NODE_ENV === 'development') {
+    base = 'http://localhost:3000';
+  } 
+  // else base = '';
 
   return base + slug;
 };
+
+// 'https://wanderlust-git-deployment.gh-wanderlust.now.sh/'

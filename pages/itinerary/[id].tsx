5727b92a3b66730e591044949e014e8141250a93
Joanna Huang <joannathhuang@gmail.com>
2020-02-14 11:46:15 -0500

    [Feat]: styling update

--- pages/itinerary/[id].tsx
+++ pages/itinerary/[id].tsx
@@ -6,8 +6,8 @@ import styled from 'styled-components';
 import Navbar from '../../components/userNavbar';
 import GoogleMapReact from 'google-map-react';
 import zipcodes from 'zipcodes';
-// import { dateFns } from 'date-fns'
-import { format } from 'url';
+import * as dateFns from 'date-fns';
+// import { format } from 'url';
 // import uuid from 'uuid';
 
 // const AnyReactComponent = ({ text }) => <Marker>{''}</Marker>;
@@ -21,71 +21,74 @@ const Itinerary = function(props: any) {
 
   const mapCoords = zipcodes.lookup(trip.listing.zipCode);
 
-  // const formattedDateFrom: Date = new Date(trip.dateFrom)
-
-  // dateFns.format(formattedDateFrom, 'MMMM dd, yyyy')
+  const formattedDateFrom: Date = new Date(trip.dateFrom);
+  const newDateFrom = dateFns.format(formattedDateFrom, 'MMMM dd, yyyy');
+  const formattedDateTo: Date = new Date(trip.dateTo);
+  const newDateTo = dateFns.format(formattedDateTo, 'MMMM dd, yyyy');
 
   return (
     <div>
       <Navbar />
-      <Wrapper>
-        <Details>
-          {console.log(trip)}
-          <h1>
-            Your upcoming {tripLengthInDays} day trip to {trip.listing.city}
-          </h1>
+      {trip.status === 'booked' ? (
+        <Wrapper>
+          <Details>
+            <div>
+              <h1>
+                Your upcoming {tripLengthInDays} day trip to {trip.listing.city}
+              </h1>
+              <Dates>
+                <IndividualDate>
+                  <PlaneImg
+                    src={
+                      'https://cdn1.iconfinder.com/data/icons/transportation-28/100/26_Airplane_take_off-512.png'
+                    }
+                  />
+                  <TravelDate>{newDateFrom}</TravelDate>
+                </IndividualDate>
+                <IndividualDate>
+                  <PlaneImg
+                    src={
+                      'https://cdn1.iconfinder.com/data/icons/transportation-28/100/27_Airplane_landing-512.png'
+                    }
+                  />
+                  <TravelDate>{newDateTo}</TravelDate>
+                </IndividualDate>
+              </Dates>
+            </div>
+            <Users>
+              {trip.users.map((user: User) => {
+                return (
+                  <Individual key={user.id}>
+                    <Link href={`/users/${user.id}`}>
+                      <UserImage src={user.imageUrl} />
+                    </Link>
+                    <UserDetail>
+                      <h3>{user.firstName}</h3>
+                      <p>{user.email}</p>
+                    </UserDetail>
+                  </Individual>
+                );
+              })}
+            </Users>
+          </Details>
+          <Accommodation>
+            <h2>Accommodation Details: </h2>
+            <h3>{trip.listing.name}</h3>
+            <Address>{`${trip.listing.address}, ${trip.listing.city}, ${trip.listing.country}`}</Address>
 
-          <Schedule>
-            <Timeline>
-              <TravelImg
-                src={
-                  'https://cdn1.iconfinder.com/data/icons/transportation-28/100/26_Airplane_take_off-512.png'
-                }
-              />
-              <TravelDate>{trip.dateFrom}</TravelDate>
-            </Timeline>
-            <Timeline>
-              <TravelImg
-                src={
-                  'https://cdn1.iconfinder.com/data/icons/transportation-28/100/27_Airplane_landing-512.png'
-                }
-              />
-              <TravelDate>{trip.dateTo}</TravelDate>
-            </Timeline>
-          </Schedule>
-          <Users>
-            {trip.users.map((user: User) => {
-              return (
-                <div>
-                  <Link key={user.id} href={`/users/${user.id}`}>
-                    <UserImage src={user.imageUrl} />
-                  </Link>
-                  <UserName>{user.firstName}</UserName>
-                </div>
-              );
-            })}
-          </Users>
-          
-          
-        </Details>
-        <div>
-          <h2>Accommodation Details: </h2>
-          <h3>{trip.listing.name}</h3>
-          <p>{`${trip.listing.address}, ${trip.listing.city}`}</p>
-          <p>{trip.listing.country}</p>
-          <ListingImg src={trip.listing.ownerPhotos[0]} />
-          <div style={{ height: '200px', width: '200px' }}>
-            <GoogleMapReact
-              bootstrapURLKeys={{
-                key: GOOGLE_MAPS_API_KEY
-              }}
-              center={{
-                lat: 44.2641,
-                lng: -72.577,
-              }}
-              defaultZoom={13}
-            >
-              {/* <Marker
+            <ListingImg src={trip.listing.ownerPhotos[0]} />
+            <div style={{ height: '200px', width: '80%' }}>
+              <GoogleMapReact
+                bootstrapURLKeys={{
+                  key: 'AIzaSyBJKCWmM_cj5A-B0H-sZF51HoSF1QOwLPU',
+                }}
+                center={{
+                  lat: 44.2641,
+                  lng: -72.577,
+                }}
+                defaultZoom={13}
+              >
+                {/* <Marker
             lat=44.2641
             lng=-72.577
             text="test marker"
@@ -93,10 +96,22 @@ const Itinerary = function(props: any) {
           >
 
           </Marker> */}
-            </GoogleMapReact>
-          </div>
+              </GoogleMapReact>
+            </div>
+          </Accommodation>
+        </Wrapper>
+      ) : (
+        <div>
+          <Redirect>
+            <h2>You currently do not have any booked trips!</h2>
+            <h3>
+              Continue to explore trips you're{' '}
+              <Link href="/accountOverview">Interested In</Link> or{' '}
+              <Link href="/listings">Find Your Next Adventure</Link>!
+            </h3>
+          </Redirect>
         </div>
-      </Wrapper>
+      )}
     </div>
   );
 };
@@ -121,19 +136,32 @@ const Wrapper = styled.div`
 `;
 
 const Details = styled.div`
-  width: 55%;
+  // width: 70%;
   // background: purple;
-  align-content: space-between;
+  // height: 100%;
+  // align-content: space-between;
+`;
+
+const Accommodation = styled.div`
+  display: column;
+  width: 40%;
 `;
 
 const ListingImg = styled.img`
-  width: 40%;
-  height: 30%;
+  width: 80%;
+  height: 40%;
 `;
 
 const Users = styled.div`
-  display: flex;
+  display: column;
+  padding: 1em;
   flex-direction: row;
+`;
+
+const Individual = styled.div`
+  display: flex;
+  width: 80%;
+  justify-content: flex-start;
 `;
 
 const UserImage = styled.img`
@@ -144,26 +172,42 @@ const UserImage = styled.img`
   padding: 5px;
 `;
 
-const UserName = styled.p``;
+const UserDetail = styled.div`
+  display: column;
+  padding: 1em;
+  width: 50%;
+  align-items: center;
+`;
 
-const Timeline = styled.div`
+const IndividualDate = styled.div`
   display: flex;
   align-items: center;
 `;
 
-const TravelImg = styled.img`
+const PlaneImg = styled.img`
   object-fit: cover;
-  border-radius: 10%;
-  height: 5vw;
-  width: 5vw;
+  padding: .5em;
+  border-radius: 20%;
+  height: 3vw;
+  width: 3vw;
 `;
 
 const TravelDate = styled.div`
   font-size: 25px;
 `;
 
-const Schedule = styled.div`
+const Dates = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
+`;
+
+const Address = styled.p`
+  display: flex;
+`;
+
+const Redirect = styled.div`
+  padding: 3em;
+  display: column;
+  justify-content: space-between;
 `;
